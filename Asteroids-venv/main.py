0ed12d0e1ed22c3c49e4d5c2d1208e42630ec64a
enav2653 <enav2653@hotmail.com>
2025-08-20 14:43:40 -0500

    add player.py, draw player triangle. Complete lesson CH2, L5

--- Asteroids-venv/main.py
+++ Asteroids-venv/main.py
@@ -1,17 +1,20 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
 	pygame.init()
 	Clock = pygame.time.Clock()
 	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	player = Player((SCREEN_WIDTH/2),(SCREEN_HEIGHT/2), PLAYER_RADIUS)
 
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill("black")
+		player.draw(screen)
 		Clock.tick(60)
 		dt = Clock.tick()/1000
 		pygame.display.flip()
